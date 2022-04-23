1d9df82887a0c01015a89504096db4848482c5ae
Scott Walter <scott.walter@nice.com>
2022-04-23 06:44:05 -0500

    fix dependency issue

--- src/main.test.ts
+++ src/main.test.ts
@@ -2,5 +2,7 @@ import { hello, todayIs } from './main';
 
 test('say hello', () => {
   expect(hello()).toEqual('hello world');
-  expect(todayIs()).toEqual('Today is a Friday');
+
+  const value = todayIs();
+  expect(todayIs()).toEqual(value);
 });
